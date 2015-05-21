3fcdbbd626a5dddaf590133b857ab92d45bf3d27
Viktor Kukurba <victor.kukurba@sethq.com>
2015-05-21 12:45:45 +0300

    change for android 4.0.0 support

--- src/android/LocalNotification.java
+++ src/android/LocalNotification.java
@@ -490,15 +490,11 @@ public class LocalNotification extends CordovaPlugin {
     */
    @TargetApi(Build.VERSION_CODES.KITKAT)
    private static void sendJavascript(final String js){
-	   webView.post(new Runnable(){
-		   public void run(){
-			   if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.KITKAT){
-				   webView.evaluateJavascript(js, null);
-			   } else {
-				   webView.loadUrl("javascript:" + js);
-			   }
-		   }
-	   });
+	   if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+       	   webView.sendJavascript(js);
+       } else {
+           webView.loadUrl("javascript:" + js);
+       }
    }
     
   
